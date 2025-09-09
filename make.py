f14d148d1cf9e7d3b30934c8bf8fc9bbfc8f0b4f
FawazOlawuyi <fawazolawuyi044@gmail.com>
2025-09-09 18:21:44 +0100

    Update make.py

--- make.py
+++ make.py
@@ -46,5 +46,4 @@ def admin():
     return render_template("admin.html", bookings=data)
 
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
